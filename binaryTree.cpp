554aa51f5ecb70f0b4e50d116b771e0942dd4594
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-26 11:49:13 +0700

    membuat void postorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -129,5 +129,26 @@ class BinaryTree
         preorder(ptr->rightchild);
     }
 
+    void postorder(Node *ptr)
+    {
+        if(isEmpty())
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr == nullptr)
+        return;
+
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " ";
+    }
+
+    bool isEmpty()
+    {
+        // check if the tree is empty
+        return ROOT == nullptr;
+    }
 
 };
