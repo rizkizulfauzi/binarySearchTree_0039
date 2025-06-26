7e655cc32cd20006afd1313c9ca3c8c40a7d4dd9
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-26 11:13:08 +0700

    instalasi pointer child ke nullptr

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -9,5 +9,8 @@ class Node
 
     //Constructor for the node class
 
-
+Node(){
+    leftchild = nullptr; //  instalalie left child to null
+    rightchild = nullptr; // instalalie right child to null
+}
 };
